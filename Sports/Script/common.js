7227114f257e2be1c795d02c93fd3781cebf2c4c
xinjin.liu <xinjin.liu@t2vsoft.com>
2014-04-23 15:44:03 +0800

    Fix js

--- Sports/Script/common.js
+++ Sports/Script/common.js
@@ -1,9 +1,20 @@
 // JavaScript Document
-$(function(){
-	//首页大图伸缩
+$(function () {
+    //首页大图伸缩
     $(".i_index_list li").hover(function () {
-	    $(this).find(".i_mask").stop().animate({ bottom: 0 }, 300);
-	},function(){
-	    $(this).find(".i_mask").stop().animate({ bottom: "-33px" }, 300);
-	})
+        $(this).find(".i_mask").stop().animate({ bottom: 0 }, 300);
+    }, function () {
+        $(this).find(".mask").stop().animate({ bottom: "-33px" }, 300);
+    })
+
+    //导航
+    jQuery("#nav").slide({
+        type: "menu",
+        titCell: ".dp",
+        targetCell: ".sub",
+        delayTime: 0,
+        defaultPlay: false,
+        returnDefault: true
+    });
+
 })
