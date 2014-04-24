4ab334ea3e3f2ec38b66a9537b641e49a2cb6776
xinjin.liu <xinjin.liu@t2vsoft.com>
2014-04-24 13:55:51 +0800

    Merge map

--- Sports/Script/common.js
+++ Sports/Script/common.js
@@ -4,7 +4,7 @@ $(function () {
     $(".i_index_list li").hover(function () {
         $(this).find(".i_mask").stop().animate({ bottom: 0 }, 300);
     }, function () {
-        $(this).find(".mask").stop().animate({ bottom: "-33px" }, 300);
+        $(this).find(".i_mask").stop().animate({ bottom: "-33px" }, 300);
     })
 
     //导航
@@ -17,4 +17,50 @@ $(function () {
         returnDefault: true
     });
 
+    //地图页高度
+    var hh = $(window).height() - 152
+    var ww = $(window).width() - 300
+    $(".result_list").css("height", hh);
+    $(".map_aside").css("height", hh - 43);
+    $("#map-container").css({ "height": hh + 109 });
+
+    $(window).on('resize', function () {
+        var hh = $(window).height() - 152
+        var ww = $(window).width() - 300
+        $(".result_list").css("height", hh);
+        $(".map_aside").css("height", hh - 43);
+        $("#map-container").css({ "height": hh + 109 });
+    })
+
+    //地图页导航
+    jQuery("#map_nav").slide({
+        type: "menu",
+        titCell: ".nav_li",
+        targetCell: ".lay-screen:not('.sublevel')",
+        delayTime: 0,
+        triggerTime: 0,
+        defaultPlay: false,
+        returnDefault: true
+    });
+
+    $(".lay-screen li").hover(function () {
+        $(this).find(".sublevel").toggle();
+    })
+
+    //地图页左侧折叠
+    $(".packup").click(function () {
+        var ts = $(this);
+        if (ts.hasClass('packed')) {
+            ts.removeClass('packed');
+            ts.prev(".map_left").stop().css({ "border-right": "1px solid #ccc", "overflow": "visible" }).animate({ "width": 300 });
+
+        } else {
+            ts.addClass('packed');
+            ts.prev(".map_left").stop().css({ "border": 0, "overflow": "hidden" }).animate({ "width": 0 });
+        }
+        //$(this).toggleClass("packed");
+        //$(".map_left").stop().css({"border":"none","overflow":"hidden"}).animate({"width":0});
+        //$(".map_left").stop().css({ width:0, border:"none", overflow:'hidden';});
+
+    })
 })
